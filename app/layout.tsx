6ec971de29ce7c7de8d2f3a776d8ec1b874381d8
EMMANUEL AYOMIDE OKUNLOLA <74790322+emmy16-glitch@users.noreply.github.com>
2026-09-09 17:08:23 +0100

    Load final check screen cleanup styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "./category-depth.css";
 import "./responsive-hardening.css";
 import "./clean-marketplace.css";
 import "./agentdesk-flow.css";
+import "./check-cleanup.css";
 import AppProviders from "@/components/providers/AppProviders";
 
 export const metadata: Metadata = {
