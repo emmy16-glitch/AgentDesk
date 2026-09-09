03665a08e4a3aaee403f2bf2ae4d8aab2457bb7c
EMMANUEL AYOMIDE OKUNLOLA <74790322+emmy16-glitch@users.noreply.github.com>
2026-09-09 01:45:15 +0100

    phase5: load category depth styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./auditions.css";
 import "./hiring.css";
+import "./category-depth.css";
 import "./responsive-hardening.css";
 import AppProviders from "@/components/providers/AppProviders";
 
